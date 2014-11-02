d686ae6756026832278e0f8e3c60c53100a78adc
shohamlan <shohamlan@gmail.com>
2014-11-02 17:34:51 +0200

    initial commit.

---
+++ src/com/mta/javacourse/ShohamlanprojectServlet.java
@@ -0,0 +1,11 @@
+package com.mta.javacourse;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ShohamlanprojectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, shoham");
+	}
+}
