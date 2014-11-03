3888ac509415be56f3f3faf12c81be84369b9233
shohamlan <shohamlan@gmail.com>
2014-11-03 18:13:33 +0200

    checking

--- src/com/mta/javacourse/ShohamlanprojectServlet.java
+++ src/com/mta/javacourse/ShohamlanprojectServlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class ShohamlanprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, shoham");
+		resp.getWriter().println("Hello, shoham from mta");
 	}
 }
