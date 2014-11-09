021ae72e60bb40ec4c10e92800d9d80b2445205e
shohamlan <shohamlan@gmail.com>
2014-11-09 17:08:22 +0200

    test commit #2

--- src/com/mta/javacourse/ShohamlanprojectServlet.java
+++ src/com/mta/javacourse/ShohamlanprojectServlet.java
@@ -7,5 +7,6 @@ public class ShohamlanprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, shoham from mta");
+		//test commit
 	}
 }
