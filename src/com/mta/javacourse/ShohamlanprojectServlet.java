8fc831f1bd25bd2de2a7718cac316db9775bb8ea
shohamlan <shohamlan@gmail.com>
2014-11-11 17:21:21 +0200

    calculation excersize 2

--- src/com/mta/javacourse/ShohamlanprojectServlet.java
+++ src/com/mta/javacourse/ShohamlanprojectServlet.java
@@ -5,8 +5,19 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class ShohamlanprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, shoham from mta");
-		//test commit
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		num1=4;
+		num2=3;
+		num3=7;
+		int result= ( num1 + num2 ) * num3;
+		String resultStr = new String("<h1>Result of ("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
+	
+		
+		
+		
 	}
 }
